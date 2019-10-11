9e07b198c9438ced399f1b7a12fb560eb827df57
Kamaljt1504 <kaurkamaljit963@gmail.com>
2019-10-11 18:37:17 -0400

    data

--- d4s/File.swift
+++ d4s/File.swift
@@ -12,19 +12,46 @@ class Student
 {
     var name:String?
     var Id : Int?
-    var marks : ["m1":68,"m2":59,"m3":78,"m4":78,"m5":78]
+    var marks : [String:Float] = ["m1":68,"m2":59,"m3":78,"m4":78,"m5":78]
     var totalMarks : Float = 0
     var per : Float
-func get
-                                       
-func Cal()
+init()
 {
-    for(_,v) in marks ?? <#default value#>
-    {
-      totalMarks = totalMarks + Float(v)
+    self.Id=0
+    self.name=""
+    self.marks=[:]
+    self.totalMarks=0.0
+    self.per = 0.0
     }
     
+    init(Id:Int,name:String,Marks:[String:Float])
+    {
+        self.Id=Id
+        self.name=name
+        self.marks=Marks
+        self.totalMarks=0.0
+        self.per = 0.0
+    }
 
+func Cal()
+{
+    for m in self.marks
+    {
+      totalMarks = totalMarks +  m.value
+    }
 }
-
+func CalPER()
+{
+    per=totalMarks/5
+}
+    
+func printData()
+{
+    print(name ?? "NAME")
+    print(Id ?? 0)
+    print(marks)
+    print(totalMarks)
+    print(per)
+    
+    }
 }
--- d4s/main.swift
+++ d4s/main.swift
@@ -9,6 +9,8 @@
 import Foundation
 
 print("Hello, World!")
-var s1=Student()
-
+var s1 = Student(Id: 101,name: "Kamal",Marks: ["m1":67,"m2":78,"m3":77,"m4":78,"m5":68])
+s1.Cal()
+s1.CalPER()
+s1.printData()
 
