600a6ad2f4c558a31f58adbc777625c0cdd8e4ab
Kamaljt1504 <kaurkamaljit963@gmail.com>
2019-10-11 18:17:23 -0400

    d4s

---
+++ d4s/File.swift
@@ -0,0 +1,30 @@
+//
+//  File.swift
+//  d4s
+//
+//  Created by MacStudent on 2019-10-11.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class Student
+{
+    var name:String?
+    var Id : Int?
+    var marks : ["m1":68,"m2":59,"m3":78,"m4":78,"m5":78]
+    var totalMarks : Float = 0
+    var per : Float
+func get
+                                       
+func Cal()
+{
+    for(_,v) in marks ?? <#default value#>
+    {
+      totalMarks = totalMarks + Float(v)
+    }
+    
+
+}
+
+}
---
+++ d4s/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  d4s
+//
+//  Created by MacStudent on 2019-10-11.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+var s1=Student()
+
+
